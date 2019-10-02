8b2f9463aa996495ade521f86a1e63ad658e0b7b
Bogdan Symchych <baltazorr@gmail.com>
2019-10-02 15:01:36 +0300

    Update NexUploadWiFi_ESP32.ino

--- examples/NexUploadWiFi_ESP32/NexUploadWiFi_ESP32.ino
+++ examples/NexUploadWiFi_ESP32/NexUploadWiFi_ESP32.ino
@@ -1,3 +1,19 @@
+/**
+ * @file NexUploadWiFi_ESP32.ino
+ *
+ * The implementation of uploading firmware to Nextion HMI displays over WiFi. 
+ *
+ * @author  Bogdan Symchych (email:<bogdan.symchych@gmail.com>)
+ * @date    2019/10/1
+ *
+ * @copyright 
+ * Copyright (C) 2019
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
 #include "NexUploadWIFI.h"
 #include <WiFi.h>
 #include <AsyncTCP.h>
